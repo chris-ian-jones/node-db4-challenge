8cc0ac4ad2ea451e72c7e389b87b83197c4f823e
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-12 21:22:29 +0100

    WIP: updaded migration file to create tables

--- data/migrations/20190912210243_firstMigration.js
+++ data/migrations/20190912210243_firstMigration.js
@@ -1,7 +1,54 @@
 exports.up = function(knex) {
-  
-};
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments()
+      tbl.string('name', 255).notNullable()
+    })
+    .createTable('step_instructions', tbl => {
+      tbl.increments()
+      tbl.integer('step_number').notNullable()
+      tbl.string('instructions', 4000).notNullable()
+      
+      //Foreign Key
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments()
+      tbl.string('name', 255).notNullable()
+      tbl.string('amount', 255).notNullable()
+    })
+    .createTable('recipe_ingredients', tbl => {
+      tbl.float('quantity').notNullable()
+
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      tbl
+        .integer('ingredient_id')
+        .unsigned()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+      tbl.primary(['recipe_id', 'ingredient_id'])
+    })
+}
 
 exports.down = function(knex) {
-  
-};
+  return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('step_instructions')
+    .dropTableIfExists('recipes')
+}
